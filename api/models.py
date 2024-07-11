f56d83c978828d54fb2ca607ffb3798384c3f2ad
HelloWorldJensen <jscheong@gmail.com>
2024-07-11 14:26:26 +1000

    added unique currency constraint

--- api/models.py
+++ api/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Currency(models.Model):
     currency_name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=3)
+    
+    constraints = [
+        models.UniqueConstraint(fields=['currency_name', 'symbol'], name='unique_currency_symbol')
+        ]    
 
     def __str__(self):
         return self.symbol
